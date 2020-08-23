5399bdff792c905c40ec409feec7079ce59e35bb
kwuzy <kevinlwu168@gmail.com>
2020-08-23 13:33:39 -0700

    added shooting and meteor

--- Assets/Dodo.cs
+++ Assets/Dodo.cs
@@ -2,10 +2,19 @@
 
 public class Dodo : MonoBehaviour
 {
+
     private void OnMouseDown()
     {
-        GetComponent<SpriteRenderer>
+        GetComponent<SpriteRenderer>().color = Color.red;     
     }
-  
-        
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnMouseDrag() {
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(newPosition.x,(float)-3.884643);
+    }
 }
