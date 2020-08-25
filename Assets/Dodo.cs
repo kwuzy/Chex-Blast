960345ee2e84bca81e61625507a9a4cca8bbf0be
kwuzy <kevinlwu168@gmail.com>
2020-08-24 17:30:29 -0700

    added walls, new meteor, experimenting with bounce

--- Assets/Dodo.cs
+++ Assets/Dodo.cs
@@ -6,15 +6,18 @@ public class Dodo : MonoBehaviour
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.red;     
+        transform.Rotate(0,0,0);
     }
 
     private void OnMouseUp()
     {
         GetComponent<SpriteRenderer>().color = Color.white;
+        transform.Rotate(0,0,0);
     }
 
     private void OnMouseDrag() {
         Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(newPosition.x,(float)-3.884643);
+        transform.Rotate(0,0,0);
     }
 }
