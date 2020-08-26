2ff4128af05e155bb33af8a9f60fde038e7b9622
kwuzy <kevinlwu168@gmail.com>
2020-08-25 17:58:32 -0700

    figured out collision

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject coconutPrefab;
+
+    public float coconutForce = 20f;
+    
+    public float fireRate = (float)0.5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            InvokeRepeating("Shoot", (float)0.001, fireRate);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            CancelInvoke("Shoot");    
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject coconut = Instantiate(coconutPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = coconut.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * coconutForce, ForceMode2D.Impulse);
+        Destroy(coconut, 5f);
+    }
+
+}
