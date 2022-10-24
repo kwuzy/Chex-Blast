7397a4df431ded2a7c8185999930c37b406af28d
kwuzy <kevinlwu168@gmail.com>
2022-10-24 11:09:49 -0400

    updated menu with clickable buttons. updated bounce on floor

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("SceneManager.GetActiveScene().buildIndex + 1");
     }
 
     public void QuitGame()
